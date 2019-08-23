3cccfb0fac32067b57f1906fe502520819bdb8b0
MauricioFT1 <maumaufreitast@gmail.com>
2019-08-23 12:11:16 -0300

    Projeto de django

---
+++ blog/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path 
+from . import views
+
+
+urlpatterns = [
+    path('post_list', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    path('', views.principal, name='principal'),
+    path('lista', views.lista, name='lista'),
+    path('brasileirao2008', views.brasileirao2008, name='brasileirao2008'),
+    path('brasileirao2018', views.brasileirao2018, name='brasileirao2018'),
+    path('copadomundo', views.copadomundo, name='copadomundo'),
+    path('jogadores', views.jogadores, name='jogadores'),
+    path('origem', views.origem, name='origem'),
+    path('regulamento', views.regulamento, name='regulamento'),
+    ]
